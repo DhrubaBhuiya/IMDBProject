696ee448870f2c70309dfc8f679cd849775db0c1
Dhruba <dhrubabhuiya@gmail.com>
2018-08-29 12:42:33 +0530

    Initial Commit

---
+++ src/main/java/com/capgemini/imdbGroup4/ImdbGroup4Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.imdbGroup4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbGroup4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbGroup4Application.class, args);
+	}
+}
