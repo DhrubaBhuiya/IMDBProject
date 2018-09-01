94b6542f5552120de045e6b673a5d0d96d02b7e4
pragnya <pragnya.prabhu@capgemini.com>
2018-09-01 09:40:21 +0530

    login

---
+++ src/main/java/com/capgemini/imdbGroup4/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.capgemini.imdbGroup4.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView init() {
+    	ModelAndView mav = new ModelAndView("home");
+    	System.out.println("Controller reached");
+		return mav;
+    }
+    
+    @RequestMapping(value = "/Contact-us", method = RequestMethod.GET)
+    public ModelAndView contact() {
+    	ModelAndView mav = new ModelAndView("Contact-us");
+		return mav;
+    }
+    @RequestMapping(value = "/About-us", method = RequestMethod.GET)
+    public ModelAndView about() {
+    	ModelAndView mav = new ModelAndView("About-us");
+		return mav;
+    }
+    @RequestMapping(value = "/Help", method = RequestMethod.GET)
+    public ModelAndView help() {
+    	ModelAndView mav = new ModelAndView("Help");
+		return mav;
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+    	ModelAndView mav = new ModelAndView("login");
+		return mav;
+    }
+}
